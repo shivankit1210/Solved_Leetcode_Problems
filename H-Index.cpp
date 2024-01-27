2260f676629673d2be115ed6810b548f8a69f715
Shivankit Agarwal <shivankit1210@gmail.com>
2024-01-27 23:48:33 +0530

    perfect solution of h-index

--- H-Index.cpp
+++ H-Index.cpp
@@ -1,12 +1,29 @@
+// class Solution {
+// public:
+//     int hIndex(vector<int>& citations) {
+
+//         sort(citations.begin(), citations.end());
+//         int mid=(citations.size())/2;
+//         if(mid==0)
+//         return 1;
+//         else
+//         return citations[mid];
+//     }
+// };
+
 class Solution {
 public:
     int hIndex(vector<int>& citations) {
 
         sort(citations.begin(), citations.end());
-        int mid=(citations.size())/2;
-        if(mid==0)
-        return 1;
-        else
-        return citations[mid];
+        int n = citations.size();
+        int maxi = 0;
+        for (int i = 0; i < n; i++) {
+            if (citations[i] >= n - i) {
+                maxi = max(maxi, n - i);
+            }
+            
+        }
+        return maxi;
     }
 };
