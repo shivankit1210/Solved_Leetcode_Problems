4f6c14b497f164f966e6685a3cacc37594f932d0
Shivankit Agarwal <shivankit1210@gmail.com>
2024-02-07 22:34:44 +0530

    updated changes

--- IntergerToRoman.cpp
+++ IntergerToRoman.cpp
@@ -3,6 +3,13 @@ public:
     string intToRoman(int num) {
         unordered_map(int, char) m;
 
+        m['1']= I;
+        m['5']= I;
+        m['1']= I;
+        m['1']= I;
+        m['1']= I;
+        m['1']= I;
+
         
 
 
