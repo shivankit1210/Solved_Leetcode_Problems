1b0dd9b1b0fd374e4d5fab87cd1b5c0ea4b601fe
Shivankit Agarwal <shivankit1210@gmail.com>
2024-02-11 10:28:16 +0530

    added updated  solution

--- LengthOfLastWord.cpp
+++ LengthOfLastWord.cpp
@@ -1,7 +1,22 @@
-class Solution {
+class Solution
+{
 public:
-    int lengthOfLastWord(string s) {
-        
-        
+    int lengthOfLastWord(string s)
+    {
+        int n = s.size();
+        int res = 0;
+        for (int i = n - 1; i >= 0; i--)
+        {
+            if (s[i] != ' ')
+            {
+                while (i >= 0 && s[i] != ' ')
+                {
+                    res++;
+                    i--;
+                }
+                return res;
+            }
+        }
+        return res;
     }
 };
