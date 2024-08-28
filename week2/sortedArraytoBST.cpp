58d0925f9ca8f3387a5280f29eacd2a8b17bb3e3
Shivankit Agarwal <shivankit1210@gmail.com>
2024-08-29 00:03:35 +0530

    Added sortedArraytoBST file

---
+++ week2/sortedArraytoBST.cpp
@@ -0,0 +1,33 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    TreeNode* sortedArrayToBST(std::vector<int>& nums) {
+        return convertToBST(nums, 0, nums.size() - 1);
+    }
+    
+private:
+    TreeNode* convertToBST(std::vector<int>& nums, int left, int right) {
+        if (left > right) {
+            return nullptr;
+        }
+        
+        int mid = left + (right - left) / 2;
+        
+        TreeNode* node = new TreeNode(nums[mid]);
+        
+        node->left = convertToBST(nums, left, mid - 1);
+        node->right = convertToBST(nums, mid + 1, right);
+        
+        return node;
+    }
+};
