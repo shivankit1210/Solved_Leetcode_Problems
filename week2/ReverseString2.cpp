a262713f0afc2a7b5366dd6f587bb26ab81c4054
Shivankit Agarwal <shivankit1210@gmail.com>
2024-09-01 08:46:51 +0530

    ADDED ReverseString2.cpp

---
+++ week2/ReverseString2.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+using namespace std;
+
+string reverseString(string str,int n){
+    for(int i=0;i<n/2;i++){
+        int temp =str[i];
+        str[i]=str[n-i-1];
+        str[n-i-1]=temp;
+}
+return str;
+}
+
+
+
+int main(){
+
+    string str ="Shivakit agarwal";
+   int n=str.length();
+    cout<<"Reverse String is"<<reverseString(str,n);
+
+    return 0;
+
+}
