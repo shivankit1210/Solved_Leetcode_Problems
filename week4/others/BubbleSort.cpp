eb9d0a796102d71a12e3b8eb97b1215c4b1c5844
Shivankit Agarwal <shivankit1210@gmail.com>
2024-09-08 13:37:10 +0530

    Correct BubbleSort.cpp logic

--- week4/others/BubbleSort.cpp
+++ week4/others/BubbleSort.cpp
@@ -1,24 +1,25 @@
 #include<iostream>
+
 using namespace std;
 
-void sorting(int array[],int n){
+void Bubble_sort(int arr[],int n){
     for(int i=0;i<n;i++){
-        for(int j=i+1;j<n;j++){
-            if(array[i]>array[j]){
-                int temp = array [i];
-                array[i]=array[j];
-                array[j]=temp;
+        for(int j=0;j<n-1-i;j++){
+            if(arr[j+1]<arr[j]){
+                int temp=arr[j];
+                arr[j]=arr[j+1];
+                arr[j+1]=temp;
             }
         }
-    }
-    for(int i=0;i<n;i++){
-        cout<<array[i]<<",";
     }
 }
 
 int main(){
-    int array[]={3,8,23,45,34,24,76,45,29,49,58,9,4};
-    int n=sizeof(array)/sizeof(array[0]);
-    sorting(array,n);
+    int arr[]={3,8,23,45,34,24,76,45,29,49,58,9,4};
+    int n= sizeof(arr)/sizeof(arr[0]);
+    Bubble_sort(arr,n);
+    for(int i=0;i<n;i++){
+        cout<<arr[i]<<",";
+    }
     return 0;
 }
