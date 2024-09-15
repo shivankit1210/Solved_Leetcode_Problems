d906a6fe3cf5c80c53f883c3bfe227c31c1ce23b
Shivankit Agarwal <shivankit1210@gmail.com>
2024-09-15 09:40:30 +0530

    optimised Code for Check2aArray.js

--- week5/others/Check2aArray.js
+++ week5/others/Check2aArray.js
@@ -1,28 +1,25 @@
-function InputArrays(arr1,arr2,n){
-    
-    //first array
-    for(var i=0;i<n;i++){
-    arr1[i]=prompt(`Enter element ${i+1} of array 1-`)
-    }
-    console.log("");
-    //second array
-    for(var j=0;j<n;j++){
-    arr2[j]=prompt(`Enter element ${j+1}-  of array 2-`)
-    }
-
-    for(let k=0;k<n;k++){
-        if(arr2[k]!=arr1[k]*arr1[k]){
-            return console.log(false);
-        }
-    }
-    return console.log(true);
-
-    
+function input(arr, n) {
+  for (var i = 0; i < n; i++) {
+    arr[i] = prompt(`Enter element ${i + 1} of array:`);
+  }
+  return arr;
 }
 
-var arr1=[];
-var arr2=[];
-var n=3;
-InputArrays(arr1,arr2,n);
+function InputArrays(n) {
+  var arr1 = [];
+  var arr2 = [];
 
+  arr1 = input(arr1, n);
+  arr2 = input(arr2, n);
 
+  for (let k = 0; k < n; k++) {
+    if (arr2[k] != arr1[k] * arr1[k]) {
+      return false;
+    }
+  }
+  return true;
+}
+
+var n = 3;
+const result=InputArrays(n);
+console.log(result);
