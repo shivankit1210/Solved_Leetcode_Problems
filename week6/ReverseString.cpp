5f580b85df6bf26d4cf67845223690205b04b3d7
Shivankit Agarwal <shivankit1210@gmail.com>
2024-09-26 08:31:49 +0530

    ADDED FirstUniqueCharacter.cpp

--- week6/ReverseString.cpp
+++ week6/ReverseString.cpp
@@ -3,6 +3,83 @@
 
 
 
+Z
+class Solution {
+public:
+    string reverseVowels(string s) {
+        int start = 0;
+        int end = s.length()-1;
+        string st = "aeiouAEIOU";
+        while(start<end){
+            bool b1 = false;
+            bool b2 = false;
+            for(char ch: st){
+                if(ch==s.at(start)) b1=true;
+            }
+            if(!b1){
+                start++;
+                continue;
+            }
+            for(char ch: st){
+                if(ch==s.at(end)) b2=true;
+            }
+            if(!b2){
+                end--;
+                continue;
+            }
+            
+            char temp = s.at(start);
+            s.at(start) = s.at(end);
+            s.at(end)= temp;
+            start++;
+            end--;
+        }
+        return s;
+    }
+};
+
+
+
+
+
+
+
+
+
+
+
+// class Solution {
+// public:
+//     string reverseVowels(string s) {
+//         int start = 0;
+//         int end = s.size() - 1;
+//         string vowels = "aeiouAEIOU";
+//         while (start < end) {
+//             // bool b1=false;
+//             // bool b2=false;
+//             for (char ch : vowels) {
+//                 if (ch != s[start]) {
+//                     start++;
+//                     continue;
+//                 }
+//             }
+//             for (char ch : vowels) {
+//                 if (ch != s[end]) {
+//                     end--;
+//                     continue;
+//                 }
+//             }
+//             swap(s[start],s[end]);
+//             start++;
+//             end--;
+//         }
+//         return s;
+//     }
+// };
+
+
+
+
 
 
 
