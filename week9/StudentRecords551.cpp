0153d8d0b159b8ac5e97943f7d0fe12f87625e99
Shivankit Agarwal <shivankit1210@gmail.com>
2024-10-01 14:59:08 +0530

    ADDED StudentRecords551.cpp

---
+++ week9/StudentRecords551.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    bool checkRecord(string s) {
+        unordered_map<char,int> CharCount;
+        int countA=0;
+        for(int i=0;i<s.length();i++){
+
+            if(s[i]=='A'){
+                countA++;
+                if(countA>=2){
+                    return false;
+                }
+            }
+            if(s[i]=='L' && s[i+1]=='L' && s[i+2]=='L'){
+                return false;
+            }
+        }
+        return true;
+        
+    }
+};
