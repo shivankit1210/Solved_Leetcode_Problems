68d3d5f694ad67a3d0e238bdb8f1c41f9b276b1f
Shivankit Agarwal <shivankit1210@gmail.com>
2024-10-10 12:32:32 +0530

    COMPLETED AssignCookies455.cpp

--- week10/AssignCookies455.cpp
+++ week10/AssignCookies455.cpp
@@ -1,3 +1,10 @@
+#include<iostream>
+#include<vector>
+#include<algorithm>
+using namespace std;
+
+
+
 class Solution {
 public:
     int findContentChildren(vector<int>& g, vector<int>& s) {
@@ -9,12 +16,22 @@ public:
         while(i<g.size() && j<s.size()){
             if(s[j]>=g[i]){
                 i++;
-                count++;
 
             }
             j++;
         }
-        return count;
+        return i;
     }
 };
+
+int main(){
+    Solution sol;
+ 
+ vector<int> g={1,2,3,7,8};
+ vector<int> s={1,6,3};
+ int res=sol.findContentChildren(g,s);
+ cout<<res;
+
+    return 0;
+}
 
