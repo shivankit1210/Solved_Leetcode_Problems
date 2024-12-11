c3bb9fd1a2e2b47cd36ec8ee7e0f721e40cdae2f
Shivankit Agarwal <shivankit1210@gmail.com>
2024-12-11 22:37:10 +0530

    ADDED mountain problem

--- week16/ValidMountain941.cpp
+++ week16/ValidMountain941.cpp
@@ -1,6 +1,31 @@
 class Solution {
 public:
     bool validMountainArray(vector<int>& arr) {
+        if(arr.size()<3){
+            return false;
+        }
+        int peak = -1;;
+        for(int i= 1;i<arr.size()-1;i++){
+            if(arr[i] > arr[i-1] && arr[i] > arr[i+1]){
+                peak = i;
+                break;
+            }
+        }
+        if(peak == -1){
+            return false;
+        }
+        for(int i =0 ; i< peak;i++){
+            if(arr[i]>= arr[i+1]){
+                return false;
+            }
+        }
+
+        for(int i=peak ; i< arr.size()-1 ; i++){
+            if(arr[i] <= arr[i+1]){
+                return false;
+            }
+        }
         
+        return true;
     }
 };
