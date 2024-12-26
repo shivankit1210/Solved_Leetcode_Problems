bfb31487e9f005e2f14bdbf5c5dd31dce9c58b7c
Shivankit Agarwal <shivankit1210@gmail.com>
2024-12-26 13:23:12 +0530

    ADDED 961 no problem

---
+++ week17/NRepeatedTerms961.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int repeatedNTimes(vector<int>& nums) {
+        unordered_map<int,int> count;
+
+        for(int num : nums){
+            count[num]++;
+
+        if(count[num] > 1){
+            return num;
+        }
+
+        }
+
+        return -1;
+
+
+        
+    }
+};
