d17477e023005eaa8b4112370da6d96739be9209
Shivankit Agarwal <shivankit1210@gmail.com>
2025-01-12 10:54:12 +0530

    ADDED 977 Solution solved using brute force

---
+++ week18/SortedArraySquare977.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    vector<int> sortedSquares(vector<int>& nums) {
+        vector<int> square;
+
+        // ******* Optimise method (__PENDING__) *******
+
+        int n =nums.size();
+        int left = 0;
+        int right = n-1;
+        int index = 
+        while()
+
+
+        // ****** brute force ******
+        // for(int i=0;i<nums.size();i++){
+        //  square.push_back(nums[i]*nums[i]);
+        // }
+        // sort(square.begin(),square.end());
+        // return square ;
+        
+    }
+};
