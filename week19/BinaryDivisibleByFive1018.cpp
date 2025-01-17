4e90627bd1b191da95bf25fdeaa75c24f5c0283d
Shivankit Agarwal <shivankit1210@gmail.com>
2025-01-17 22:51:31 +0530

    ADDED 1030 problem

--- week19/BinaryDivisibleByFive1018.cpp
+++ week19/BinaryDivisibleByFive1018.cpp
@@ -1,3 +1,15 @@
+//____________Problem Statement______________
+
+// 1018. Binary Prefix Divisible By 5
+
+// You are given a binary array nums (0-indexed).
+
+// We define xi as the number whose binary representation is the subarray nums[0..i] (from most-significant-bit to least-significant-bit).
+
+// For example, if nums = [1,0,1], then x0 = 1, x1 = 2, and x2 = 5.
+// Return an array of booleans answer where answer[i] is true if xi is divisible by 5.
+
+
 class Solution {
 public:
     vector<bool> prefixesDivBy5(vector<int>& nums) {
@@ -12,3 +24,9 @@ public:
        
     }
 };
+
+
+
+
+
+
